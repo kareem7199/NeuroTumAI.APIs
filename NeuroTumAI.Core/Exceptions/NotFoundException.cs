5b1c889368b0930be8b4fb2260e4b8ad3783dcbf
Kareem Tamer <kareemtameregy@gmail.com>
2025-03-09 20:23:45 +0200

    Error-Handling
    git rebase --continue

---
+++ NeuroTumAI.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroTumAI.Core.Exceptions
+{
+	public class NotFoundException : ApplicationException
+	{
+		public NotFoundException(string name, object key) :
+			base($"{name} with Id : {key} is not found")
+		{
+
+		}
+	}
+}
