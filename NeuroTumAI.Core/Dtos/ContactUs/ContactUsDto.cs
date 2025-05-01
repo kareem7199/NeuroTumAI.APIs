c343173a8b6ada6e451b8a801c82921b106f3f18
NouranMousa2609 <nouranmousa55@gmail.com>
2025-05-01 17:02:15 +0300

    Contact Us EndPoint

---
+++ NeuroTumAI.Core/Dtos/ContactUs/ContactUsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroTumAI.Core.Dtos.ContactUs
+{
+    public class ContactUsDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter You Message")]
+        [StringLength(1000, ErrorMessage = "Message Length")]
+        public string Message { get; set; }
+
+
+    }
+}
