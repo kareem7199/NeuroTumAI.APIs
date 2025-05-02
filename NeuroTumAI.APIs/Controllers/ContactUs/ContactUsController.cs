94847b0fdfd73beec3cebead27cd24e2db002503
kareem7199 <kareemtameregy@gmail.com>
2025-05-02 16:29:52 +0300

    Some changes

--- NeuroTumAI.APIs/Controllers/ContactUs/ContactUsController.cs
+++ NeuroTumAI.APIs/Controllers/ContactUs/ContactUsController.cs
@@ -13,22 +13,23 @@ namespace NeuroTumAI.APIs.Controllers.ContactUs
     public class ContactUsController : BaseApiController
     {
         private readonly IContactUsService _ContactUsService;
-        
+		private readonly IMapper _mapper;
 
-        public ContactUsController(IContactUsService ContactUsService)
+		public ContactUsController(IContactUsService ContactUsService, IMapper mapper)
         {
             _ContactUsService = ContactUsService;
-       
-        }
+			_mapper = mapper;
+		}
 
         [HttpPost]
         [Authorize(Roles = "Patient")]
-        public async Task<ActionResult<ContactUS>> SendMessage(ContactUsDto model)
+        public async Task<ActionResult<ContactUsMessageToReturnDto>> SendMessage(ContactUsDto model)
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            var review = await _ContactUsService.SendMessageAsync(model, userId);
-            return Ok();
+            var message = await _ContactUsService.SendMessageAsync(model, userId);
+            
+            return Ok(_mapper.Map<ContactUsMessageToReturnDto>(message));
         }
     }
 }
