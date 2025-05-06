a69262b48c2d21d5d682152bf0398efec0e31b62
kareem7199 <kareemtameregy@gmail.com>
2025-05-06 23:04:33 +0300

    Integrate AI-based cancer detection in CancerDetectionService

---
+++ NeuroTumAI.APIs/Controllers/CancerPrediction/CancerPredictionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NeuroTumAI.APIs.Controllers.Base;
+using NeuroTumAI.Core.Dtos.CancerPrediction;
+using NeuroTumAI.Core.Services.Contract;
+using NeuroTumAI.Service.Services.BlobStorageService;
+
+namespace NeuroTumAI.APIs.Controllers.CancerPrediction
+{
+	public class CancerPredictionController : BaseApiController
+	{
+		private readonly ICancerDetectionService _cancerDetectionService;
+		private readonly IBlobStorageService _blobStorageService;
+
+		public CancerPredictionController(ICancerDetectionService cancerDetectionService, IBlobStorageService blobStorageService)
+		{
+			_cancerDetectionService = cancerDetectionService;
+			_blobStorageService = blobStorageService;
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<CancerPredictionResultDto>> PredictCancer([FromForm] PredictRequestDto model)
+		{
+			using var stream = model.Image.OpenReadStream();
+			var fileUrl = await _blobStorageService.UploadFileAsync(stream, model.Image.FileName, "patient-cancer-images");
+
+			var result = await _cancerDetectionService.PredictCancerAsync(fileUrl);
+
+			return Ok(result);
+		}
+	}
+}
