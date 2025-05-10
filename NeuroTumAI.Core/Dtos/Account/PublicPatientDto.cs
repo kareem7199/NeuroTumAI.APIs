85a7728a9002485c278f383104fa2d427e070418
kareem7199 <kareemtameregy@gmail.com>
2025-05-10 22:28:59 +0300

    Update PublicPatientDto

--- NeuroTumAI.Core/Dtos/Account/PublicPatientDto.cs
+++ NeuroTumAI.Core/Dtos/Account/PublicPatientDto.cs
@@ -8,7 +8,7 @@ namespace NeuroTumAI.Core.Dtos.Account
 {
 	public class PublicPatientDto 
 	{
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string FullName { get; set; }
         public string? ProfilePicture { get; set; }
 		public DateTime DateOfBirth { get; set; }
